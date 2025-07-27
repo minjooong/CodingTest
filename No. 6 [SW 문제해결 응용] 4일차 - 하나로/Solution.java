7d55e7c9d38fa15d507dc11eb927343b47c08b54
Kim Minjung <kmj13675@gmail.com>
2025-07-27 21:46:48 +0900

    No. 6 [SW 문제해결 응용] 4일차 - 하나로

---
+++ No. 6 [SW 문제해결 응용] 4일차 - 하나로/Solution.java
@@ -0,0 +1,121 @@
+/////////////////////////////////////////////////////////////////////////////////////////////
+// 기본 제공코드는 임의 수정해도 관계 없습니다. 단, 입출력 포맷 주의
+// 아래 표준 입출력 예제 필요시 참고하세요.
+// 표준 입력 예제
+// int a;
+// double b;
+// char g;
+// String var;
+// long AB;
+// a = sc.nextInt();                           // int 변수 1개 입력받는 예제
+// b = sc.nextDouble();                        // double 변수 1개 입력받는 예제
+// g = sc.nextByte();                          // char 변수 1개 입력받는 예제
+// var = sc.next();                            // 문자열 1개 입력받는 예제
+// AB = sc.nextLong();                         // long 변수 1개 입력받는 예제
+/////////////////////////////////////////////////////////////////////////////////////////////
+// 표준 출력 예제
+// int a = 0;                            
+// double b = 1.0;               
+// char g = 'b';
+// String var = "ABCDEFG";
+// long AB = 12345678901234567L;
+//System.out.println(a);                       // int 변수 1개 출력하는 예제
+//System.out.println(b); 		       						 // double 변수 1개 출력하는 예제
+//System.out.println(g);		       						 // char 변수 1개 출력하는 예제
+//System.out.println(var);		       				   // 문자열 1개 출력하는 예제
+//System.out.println(AB);		       				     // long 변수 1개 출력하는 예제
+/////////////////////////////////////////////////////////////////////////////////////////////
+import java.util.*;
+import java.io.FileInputStream;
+
+/*
+   사용하는 클래스명이 Solution 이어야 하므로, 가급적 Solution.java 를 사용할 것을 권장합니다.
+   이러한 상황에서도 동일하게 java Solution 명령으로 프로그램을 수행해볼 수 있습니다.
+ */
+class Solution
+{
+    static int[] parents, x, y;
+    static ArrayList<Map.Entry<Long, int[]>> cost;
+    static long ans;
+
+    static int find(int a) {
+        if (parents[a] == a) return a;
+        return find(parents[a]);
+    }
+    static void union(long c, int a, int b) {
+        int pa = find(a);
+        int pb = find(b);
+        if (pa != pb) {
+            parents[pb] = pa;
+            ans += c;
+        }
+    }
+
+	public static void main(String args[]) throws Exception
+	{
+		/*
+		   아래의 메소드 호출은 앞으로 표준 입력(키보드) 대신 input.txt 파일로부터 읽어오겠다는 의미의 코드입니다.
+		   여러분이 작성한 코드를 테스트 할 때, 편의를 위해서 input.txt에 입력을 저장한 후,
+		   이 코드를 프로그램의 처음 부분에 추가하면 이후 입력을 수행할 때 표준 입력 대신 파일로부터 입력을 받아올 수 있습니다.
+		   따라서 테스트를 수행할 때에는 아래 주석을 지우고 이 메소드를 사용하셔도 좋습니다.
+		   단, 채점을 위해 코드를 제출하실 때에는 반드시 이 메소드를 지우거나 주석 처리 하셔야 합니다.
+		 */
+		System.setIn(new FileInputStream("re_sample_input.txt"));
+
+		/*
+		   표준입력 System.in 으로부터 스캐너를 만들어 데이터를 읽어옵니다.
+		 */
+		Scanner sc = new Scanner(System.in);
+		int T;
+		T=sc.nextInt();
+		/*
+		   여러 개의 테스트 케이스가 주어지므로, 각각을 처리합니다.
+		*/
+
+		for(int test_case = 1; test_case <= T; test_case++)
+		{
+		
+			/////////////////////////////////////////////////////////////////////////////////////////////
+			/*
+				 이 부분에 여러분의 알고리즘 구현이 들어갑니다.
+			 */
+			/////////////////////////////////////////////////////////////////////////////////////////////
+
+            int N = sc.nextInt(); // 섬의 개수
+            parents = new int[N+1];
+            x = new int[N+1];
+            y = new int[N+1];
+
+            for (int i=1; i <= N; i++) {
+                x[i] = sc.nextInt();
+                parents[i] = i;
+            }
+            for (int i=1; i <= N; i++) {
+                y[i] = sc.nextInt();
+            }
+           
+            double E = sc.nextDouble();
+
+            cost = new ArrayList<Map.Entry<Long, int[]>>();
+
+            for (int i=1; i <= N; i++) {
+                for (int j=1; j <= N; j++) {
+                    if (i == j) continue;
+                    long dx = (long)(x[i] - x[j]);
+                    long dy = (long)(y[i] - y[j]);
+                    long c = dx * dx + dy * dy;
+                    cost.add(new AbstractMap.SimpleEntry<>(c, new int[] {i, j}));
+                }
+            }
+            cost.sort(Comparator.comparingLong(Map.Entry::getKey));
+
+            ans = 0;
+            for (Map.Entry<Long, int[]> entry : cost) {
+                union(entry.getKey(), entry.getValue()[0], entry.getValue()[1]);
+            }
+            ans = (long) Math.round(ans * E);
+
+            System.out.printf("#%s %s\n", test_case, ans);
+		}
+	}
+}
