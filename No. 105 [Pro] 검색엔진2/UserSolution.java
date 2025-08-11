e8e121a4c8108fc329fe7bba3e10eb78507686ee
Kim Minjung <kmj13675@gmail.com>
2025-08-12 02:23:19 +0900

    No. 105 [Pro] 검색엔진2

---
+++ No. 105 [Pro] 검색엔진2/UserSolution.java
@@ -0,0 +1,28 @@
+class UserSolution
+{
+	void init()
+	{
+		return;
+	}
+
+	void search(char mStr[], int mCount)
+	{
+		return;
+	}
+
+	Solution.Result recommend(char mStr[])
+	{
+		Solution.Result res = new Solution.Result();
+		return res;
+	}
+
+	int relate(char mStr1[], char mStr2[])
+	{
+		return 0;
+	}
+
+	void rank(char mPrefix[], int mRank, char mReturnStr[])
+	{
+		return;
+	}
+}
