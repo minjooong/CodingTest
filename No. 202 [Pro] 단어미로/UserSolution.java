c7d24fad94fe5813bf4f206b44b4b91c708f24dc
Kim Minjung <kmj13675@gmail.com>
2025-08-19 18:10:47 +0900

    No. 202 [Pro] 단어미로 - 문제

---
+++ No. 202 [Pro] 단어미로/UserSolution.java
@@ -0,0 +1,22 @@
+class UserSolution {	
+	void init()
+	{
+	}
+
+	void addRoom(int mID, char mWord[], int mDirLen[])
+	{
+	}
+
+	void setCurrent(char mWord[])
+	{
+	}
+
+	int moveDir(int mDir)
+	{
+		return 0;
+	}
+
+	void changeWord(char mWord[], char mChgWord[], int mChgLen[])
+	{
+	}
+}
