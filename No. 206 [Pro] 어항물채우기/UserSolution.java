ee783fab412a9a3cdfdca06d2538c469f44abbfc
Kim Minjung <kmj13675@gmail.com>
2025-08-28 23:36:47 +0900

    No. 206 [Pro] 어항물채우기 - 문제

---
+++ No. 206 [Pro] 어항물채우기/UserSolution.java
@@ -0,0 +1,19 @@
+class UserSolution {
+
+	public void init(int N, int mWidth, int mHeight, int mIDs[], int mLengths[][], int mUpShapes[][]) {
+	}
+
+	public int checkStructures(int mLengths[], int mUpShapes[], int mDownShapes[]) {
+		return 0;
+	}
+
+	public int addStructures(int mLengths[], int mUpShapes[], int mDownShapes[]) {
+		return 0;
+	}
+
+	public Solution.Result pourIn(int mWater) {
+		Solution.Result ret = new Solution.Result();
+		ret.ID = ret.height = ret.used = 0;
+		return ret;
+	}
+}
