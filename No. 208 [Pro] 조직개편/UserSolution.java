459ad9a49ab0aca7b7fa37998f76d151b9791170
Kim Minjung <kmj13675@gmail.com>
2025-09-05 23:51:40 +0900

    No. 208 [Pro] 조직개편 - 문제

---
+++ No. 208 [Pro] 조직개편/UserSolution.java
@@ -0,0 +1,17 @@
+class UserSolution {
+    public void init(int mId, int mNum) {
+        return;
+    }
+
+    public int add(int mId, int mNum, int mParent) {
+        return 0;
+    }
+
+    public int remove(int mId) {
+        return 0;
+    }
+
+    public int reorganize(int M, int K) {
+        return 0;
+    }
+}
